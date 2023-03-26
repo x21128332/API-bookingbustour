b8abcce572668edeec75940cdff5179273df0ff0
Aisling Kearney <x21128332@student.ncirl.ie>
2023-03-26 15:38:58 +0100

    fixing internal server error

--- sqlqueries.sql
+++ sqlqueries.sql
@@ -5,14 +5,16 @@ CREATE USER [aislingsbustours-bookingapi/slots/staging] FROM EXTERNAL PROVIDER;
 ALTER ROLE db_datareader ADD MEMBER [aaislingsbustours-bookingapi/slots/staging];
 GRANT SELECT TO [aislingsbustours-bookingapi/slots/staging];
 GRANT EXECUTE ON [dbo].[timetable_procedure] TO [aislingsbustours-bookingapi/slots/staging];
-GRANT EXECUTE ON [dbo].[get_booking_procedure] TO [aislingsbustours-bookingapi];
-
+GRANT EXECUTE ON [dbo].[get_booking_procedure] TO [aislingsbustours-bookingapi/slots/staging];
+GRANT EXECUTE ON SCHEMA::dbo TO [aislingsbustours-bookingapi/slots/staging];
 
 CREATE USER [aislingsbustours-bookingapi] FROM EXTERNAL PROVIDER;
 ALTER ROLE db_datareader ADD MEMBER [aislingsbustours-bookingapi];
 GRANT SELECT TO a[aislingsbustours-bookingapi];
 GRANT EXECUTE ON [dbo].[timetable_procedure] TO [aislingsbustours-bookingapi];
 GRANT EXECUTE ON [dbo].[get_booking_procedure] TO [aislingsbustours-bookingapi];
+GRANT EXECUTE ON SCHEMA::dbo TO [aislingsbustours-bookingapi];
+
 
 /*creating store PROCEDURE */
 CREATE PROCEDURE timetable_procedure
