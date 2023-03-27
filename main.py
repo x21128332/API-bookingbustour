8e00d201b9eb4241cab5af66b032b28fc2df1693
Aisling Kearney <x21128332@student.ncirl.ie>
2023-03-27 11:43:54 +0100

    testing booking_id view

--- main.py
+++ main.py
@@ -37,14 +37,33 @@ def view_bookings():
 
 @app.get('/bookings/{booking_id}')
 def get_booking(booking_id: int):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    #cursor.execute("SELECT * FROM Bookings WHERE BookingId=?", booking_id)
-    cursor.execute("EXEC [dbo].[search_booking_procedure] @booking_id = ?", booking_id)
-    booking = cursor.fetchone()
-    cursor.close()
-
-    if not booking:
-        return {'error': 'Booking not found'}
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        # adding the sql stored procedure script and parameter values
+        stored_proc = "[dbo].[search_booking_procedure] @booking_id = ?"
+        params = (booking_id)
+        # Execute stored procedur with the params
+        cursor.execute(stored_proc, params)
     
-    return {'booking_id': booking.booking_id}
+        # Iterate the cursor
+        row = cursor.fetchone()
+        while row:
+            print(str(row[0]) + " : " + str(row[1] or '') )
+            row = cursor.fetchone()
+        cursor.close()
+        del cursor
+        conn.close()
+
+    except Exception as e:
+        print("Error: %s" % e)
+
+    # #cursor.execute("SELECT * FROM Bookings WHERE BookingId=?", booking_id)
+    # cursor.execute("EXEC [dbo].[search_booking_procedure] @booking_id = ?", booking_id)
+    # booking = cursor.fetchone()
+    # cursor.close()
+
+    # if not booking:
+    #     return {'error': 'Booking not found'}
+    
+    # return {'booking_id': booking.booking_id}
