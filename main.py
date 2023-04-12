0229b27c31a5c4331cf033799e4b31c1dd8f340d
Aisling Kearney <x21128332@student.ncirl.ie>
2023-04-12 18:59:33 +0100

    adding getbooking/bookingid

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ def view_bookings():
     return {"bookings": bookings}
 
 # view a specific booking
-@app.get('/bookings/{booking_id}')
+@app.get('/getbookings/{booking_id}')
 def get_booking(booking_id: int):
     try:
         conn = get_db_connection()
